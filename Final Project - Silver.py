729e2d92f4101369151280b09a4de018fc492f46
glauco <glauco>
2024-10-15 12:21:20 +0000

    Improves Silver draft a bit more

--- Final Project - Silver.py
+++ Final Project - Silver.py
@@ -59,7 +59,7 @@ display(homes_finished)
 
 # COMMAND ----------
 
-# "107" is the code Barcelona neighborhoods
+# "107" is the code for Barcelona neighborhoods
 # "-1" is the annual average price
 def create_barcelona_avg_monthly_rental_prices(average_monthly_rental_prices):
     return average_monthly_rental_prices\
@@ -69,7 +69,44 @@ def create_barcelona_avg_monthly_rental_prices(average_monthly_rental_prices):
         .withColumn('trimester', col('trimester').cast('int'))\
         .withColumn('amount', col('amount').cast('double'))\
         .join(barcelona_neighborhoods, barcelona_neighborhoods.code == average_monthly_rental_prices.inner_scope)\
-        .drop('outer_scope', 'inner_scope')
+        .withColumnRenamed('name', 'neighborhood')\
+        .drop('outer_scope', 'inner_scope', 'code')
 
 barcelona_avg_monthly_rental_prices = create_barcelona_avg_monthly_rental_prices(average_monthly_rental_prices)
 display(barcelona_avg_monthly_rental_prices)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Process Barcelona Homes Started
+
+# COMMAND ----------
+
+# "105" is the code for cities in Catalonia
+# "8019" is the code for Barcelona
+# TODO: Some years are pending the annual aggregated value, we have to calculate it ourselves
+def create_barcelona_homes_started(homes_started):
+    return homes_started\
+        .filter(col('outer_scope') == '105')\
+        .filter(col('inner_scope') == '8019')
+
+barcelona_homes_started = create_barcelona_homes_started(homes_started)
+display(barcelona_homes_started)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## Process Barcelona Homes Finished
+
+# COMMAND ----------
+
+# "105" is the code for cities in Catalonia
+# "8019" is the code for Barcelona
+# TODO: Some years are pending the annual aggregated value, we have to calculate it ourselves
+def create_barcelona_homes_finished(homes_finished):
+    return homes_finished\
+        .filter(col('outer_scope') == '105')\
+        .filter(col('inner_scope') == '8019')
+
+barcelona_homes_finished = create_barcelona_homes_finished(homes_finished)
+display(barcelona_homes_finished)
